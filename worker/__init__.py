f028abd1e85ed2f1908b0ee84ce91fedda0a53b3
Roni Dover <roni.dover@gmail.com>
2022-05-14 00:08:33 -0700

    fixed import order

--- worker/__init__.py
+++ worker/__init__.py
@@ -1,2 +1,2 @@
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(verbose=True)
--- worker/main.py
+++ worker/main.py
@@ -1,10 +1,7 @@
 from dotenv import load_dotenv
 from opentelemetry.instrumentation.logging import LoggingInstrumentor
-
 from queueing import create_queue_channel
-load_dotenv(verbose=True)
-import pathlib
-import pika, sys, os
+import sys, os
 import logging
 from opentelemetry.instrumentation.psycopg2 import Psycopg2Instrumentor
 from opentelemetry.sdk.resources import Resource, SERVICE_NAME
@@ -12,18 +9,20 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
-from config import get_settings
 from goblin_worker import goblins_wait_for_work
+from opentelemetry.instrumentation.digma import DigmaConfiguration
+from config import get_settings
 
 LOGLEVEL = os.environ.get('LOGLEVEL', 'WARNING').upper()
 logging.basicConfig(level=LOGLEVEL)
 
 
 def main():
-
     settings = get_settings()
 
-    resource = Resource.create(attributes={SERVICE_NAME: 'vault_services'})
+    resource = Resource.create(attributes={SERVICE_NAME: 'goblin_worker'})
+    resource = DigmaConfiguration().trace_this_package()\
+                    .set_environment("Dev").resource.merge(resource)
     exporter = OTLPSpanExporter(endpoint=settings.otlp_exporter_url, insecure=True)
     provider = TracerProvider(resource=resource)
     provider.add_span_processor(BatchSpanProcessor(exporter))
@@ -38,7 +37,7 @@ if __name__ == '__main__':
     try:
         main()
     except Exception as e:
-        print (e.message)
+        print (str(e))
     except KeyboardInterrupt:
         print('Interrupted')
         try:
