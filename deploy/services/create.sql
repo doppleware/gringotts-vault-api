e7c2b84238cecce5e2cfb17bc9e45f7bd75b272e
Roni Dover <roni.dover@gmail.com>
2022-05-14 17:00:54 -0700

    moving files around

--- deploy/services/create.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS devdb WITH (FORCE);
-CREATE DATABASE devdb;
-\connect devdb;
-CREATE SCHEMA gringotts;
-
-DROP DATABASE IF EXISTS testdb WITH (FORCE);
-CREATE DATABASE testdb;
-\connect testdb;
-CREATE SCHEMA gringotts;
